d7611476a970f072ac225a00e09db78913691247
d.sedletsky@gmail.com <dsedlets@c2r13s3.42yerevan.am>
2024-07-08 14:54:27 +0400

    first

---
+++ initiation.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   initiation.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsedlets <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/08 13:51:02 by dsedlets          #+#    #+#             */
+/*   Updated: 2024/07/08 14:33:56 by dsedlets         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	if_valid(char *str)
+{
+	long int	i;
+
+	i = 0;
+	if (str[i] == '-' && str[i + 1])
+		i++;
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			exit (1 && write(2, "Error\n", 6));
+		i++;
+	}
+	return (1);
+}
+
+int	uniq(int n, t_s *a)
+{
+	while (a)
+	{
+		if (a->num == n)
+			exit (1 && write(2, "Error\n", 6));
+		a = a->next;
+	}
+	return (1);
+}
+
+int	try_add(char *current_str, t_s **a)
+{
+	int	overflow;
+	int	num;
+	t_s	*new_node;
+
+	overflow = 0;
+	if (if_valid(current_str))
+	{
+		num = ft_atoi(current_str, &overflow);
+		if (uniq(num, *a))
+		{
+			if (overflow)
+				exit (1, && write(2, "Error\n", 6));
+			if (!overflow)
+			{
+				new_node = ft_lstnew(num);
+				if (!new_node)
+					return (free_lst(a));
+				ft_lstadd_back(a, new_node);
+				return (1);
+			}
+		}
+	}
+	return (0);
+}
+
+int	init_a(char **av, t_s **a)
+{
+	int		i;
+	int		j;
+	char	**current_array;
+
+	i = 0;
+	if (!av[1])
+		return (0);
+	while (av[i])
+	{
+		current_array = ft_split(av[i], ' ');
+		j = 0;
+		while (current_array[j])
+		{
+			if (!try_add(current_array[j], a))
+			{
+				free_array(current_array);
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
---
+++ push_swap.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dsedlets <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 23:08:11 by dsedlets          #+#    #+#             */
+/*   Updated: 2024/07/08 14:34:14 by dsedlets         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int ac, char **av)
+{
+	t_s	*a;
+	t_s	*b;
+	t_s	*tmp;
+	int	max_index;
+
+	if (ac == 1 || (ac == 2 && !av[1][0]))
+		exit (1 && write(2, "Error\n", 6));
+	max_index = init_a(av, &a);
+	make_indexes_for_struct(a);
+	max_index = list_lenght(a) - 1;
+	b = NULL;
+	if (corner_case(a, max_index))
+		return (0);
+	tmp = clone_struct(a);
+	if (if_close_to_sorted(tmp, max_index, 0, max_index))
+	{
+		almost_sorted(&a);
+		return (0);
+	}
+	free_lst(&tmp);
+	low_num(&a, &b, max_index);
+	butterfly_sort(&a, &b);
+	back_to_a(&a, &b, max_index);
+	return (0);
+}
